bb73445734b2ff77f0c06ebaf19a040e9e2b6f09
Sascha Bast <sash@mischkonsum.org>
2021-03-19 19:52:15 +0100

    a

--- etc/objects/send.sh
+++ etc/objects/send.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 curl -vX POST http://localhost:4343 -d @slash_public.json \
 --header "Content-Type: application/json"
--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
- pip install pillow google-auth pygments google-auth google-auth-httplib2 google-api-python-client google-api-python-client flask
+#!/usr/bin/env bash
+pip3 install \
+pillow \
+google-auth \
+pygments \
+google-auth \
+google-auth-httplib2 \
+google-api-python-client \
+google-api-python-client \
+flask \
+uuid
 
 

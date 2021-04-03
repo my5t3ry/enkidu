d07fb4863be5e9e5ccecf91a187a745c31765eb5
Sascha Bast <sash@mischkonsum.org>
2021-04-04 00:51:44 +0200

    a

--- components/settings_repository/SettingsRepository.py
+++ components/settings_repository/SettingsRepository.py
@@ -11,14 +11,14 @@ class SettingsRepository:
       return json.loads(db[user_name]) if user_name in db else json.loads(
           self.init_default_settings(
               db,
-              user_name))
+              user_name, "unknown player one"))
 
-  def get_settings(self, user_name, display_name):
+  def get_settings_and_init(self, user_name, display_name):
     with dbm.open(ConstantsService.get_value('db_path'), 'c') as db:
       return json.loads(db[user_name]) if user_name in db else json.loads(
           self.init_default_settings(
               db,
-              user_name,display_name))
+              user_name, display_name))
 
   def get_setting_value(self, user_name, key):
     settings = self.get_settings(user_name)
@@ -30,7 +30,7 @@ class SettingsRepository:
 
   def init_default_settings(self, db, user_name, display_name):
     with open(ConstantsService.get_value('default_settings_path')) as json_file:
-      default_settings_json = json_file.read()
-      default_settings_json["display_name"] = display_name
-      db[user_name] = default_settings_json
-      return default_settings_json
+      default_settings = json.loads(json_file.read())
+      default_settings["display_name"] = display_name
+      db[user_name] = json.dump(default_settings)
+      return default_settings

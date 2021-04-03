3fbd2bf0fd4af29ed6fd668690c718156e9450ce
Sascha Bast <sash@mischkonsum.org>
2021-04-04 01:55:29 +0200

    a

--- task/net/Net.py
+++ task/net/Net.py
@@ -1,8 +1,10 @@
 from task.net.NetPingTask import NetPingTask
+from task.net.NetScanTask import NetScanTask
 from task.net.NetWhoisTask import NetWhoisTask
 
 net_registry = {
   '--whois': NetWhoisTask,
   '--ping': NetPingTask,
+  '--scan': NetScanTask,
   'default': NetPingTask,
 }
---
+++ task/net/NetScanTask.py
@@ -0,0 +1,18 @@
+import subprocess
+
+from task.PrivatTask import PrivatTask
+
+
+class NetScanTask(PrivatTask):
+
+  def __init__(self, event, payload):
+    super(NetScanTask, self).__init__(event, payload)
+
+  def run(self):
+    pass
+
+  def get_message(self):
+    result = subprocess.check_output(
+        "nmap -v -sP " + self.payload, shell=True)
+    return {
+      "text": "```\n" + result.decode("utf-8") + "\n```"}
